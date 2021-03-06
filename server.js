68f9aa8f8c71df640f29fdf209e32593f8fb2109
Steven <sastudillo6@gmail.com>
2021-03-06 14:31:37 -0500

    Testing another route, and configured HotShot for Express server.

--- server.js
+++ server.js
@@ -8,3 +8,7 @@ app.listen(3000, console.log('Server is running on port 3000'));
 app.get('/' , (req, res) => {
     res.send("Hello I am running express server.")
 });
+
+app.get('/drinks', (req, res) => {
+    res.send('This is drinks page.')
+});
