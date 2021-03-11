40f23c06f6b63328faa728da9a4413e6b3875b8a
Steven <sastudillo6@gmail.com>
2021-03-11 14:24:32 -0500

    Trying to fix Heroku deploy.

--- server.js
+++ server.js
@@ -17,6 +17,19 @@ app.listen(PORT, console.log(`Express server running on port ${PORT}`));
 
 connectionDB();
 
+app.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin", "*")
+    // res.header("Access-Control-Allow-Credentials: true") 
+    res.header("Access-Control-Allow-Headers", 
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+    // res.header("Access-Control-Max-Age", "1000")
+    if (req.method == "OPTIONS"){
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET")
+        return res.status(200).json({})
+    }
+    next()
+})
+
 // app.use(express.json())
 
 // Primary Routes
